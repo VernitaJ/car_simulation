b9a7270229ddc9603fa0f8d5cd554e0e0140bc39
Drake Axelrod <gusaxedr@student.gu.se>
2021-04-20 14:48:18 +0200

    Add discord bot to control car
    Commands include drive, help, go, gor, gol, rr, rrr, rrl, boost.
    More functionality can be added at a later date if desired. This
    is a basic implementation.

--- python/discord/bot.py
+++ python/discord/bot.py
@@ -5,34 +5,40 @@ import os
 import sys
 import dotenv
 import asyncio
+import time
 from dotenv import load_dotenv
 dir_path = os.path.dirname(os.path.realpath(__file__))
 parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
 sys.path.insert(0, parent_dir_path)
-from connection import connecter, Topic
+from connection import Topic, connecter
 
 
 load_dotenv()
 
 TOKEN = os.getenv('DISCORD_TOKEN')
-mqtt = connecter.create("Discord Bot")
-t = Topic()
-VELOCITY = 0
-DIRECTION = 0
-client = commands.Bot(command_prefix="!", help_command=None)
 
+mqtt = connecter.create("Discord Bot")
+
+t = Topic()
+
+VELOCITY = 50
+DIRECTION = 50
+
+client = commands.Bot(command_prefix=".", help_command=None)
 
 @client.command()
 async def help(ctx):
     embed = discord.Embed(colour=discord.Colour(
         0xf5a623), timestamp=datetime.datetime.utcnow())
-    embed.add_field(name="commands", value="desc", inline=False)
+    embed.add_field(name=".drive", value="the drive command takes a string of < (left), > (right), f (forward), r (reverse), b (boost), s (stop) and does each of them for one second. \n[Example: .drive <f>b>s>f]", inline=False)
+    embed.add_field(name=".go", value="forward", inline=False)
+    embed.add_field(name=".gor", value="forward & right", inline=False)
+    embed.add_field(name=".gol", value="forward & left", inline=False)
+    embed.add_field(name=".rr", value="reverse", inline=False)
+    embed.add_field(name=".rrl", value="reverse & left", inline=False)
+    embed.add_field(name=".rrr", value="reverse & right", inline=False)
+    embed.add_field(name=".s", value="stop", inline=False)
     await ctx.send(embed=embed)
-
-# for filename in os.listdir('./cogs'): #loads all files (*.py)
-#     if filename.endswith('.py'):
-#         client.load_extension(f'cogs.{filename[:-3]}') #loads the file without ".py" for example: cogs.ping
-#         print(f'Loaded {filename[:-3]}')
 
 # def seperateArgs(args, delimeter):
 #     finalArgs = []
@@ -50,49 +56,87 @@ async def help(ctx):
 #         index += 1
 #     return finalArgs
 
-async def increment(i):
-  return i + 1
 
-async def decrement(i):
-  return i - 1
-
-async def drive(v, d):
+async def control(v, d):
   if v > 0:
     mqtt.publish(t.throttle_forward, str(v))
-  elif d < 0:
+  elif v < 0:
     mqtt.publish(t.throttle_reverse, str(v))
   else:
     mqtt.publish(t.throttle_reverse, "0")
   if d > 0:
     mqtt.publish(t.steering_right, str(d))
+    time.sleep(1)
+    mqtt.publish(t.steering_right, "0")
   elif d < 0:
     mqtt.publish(t.steering_left, str(d))
+    time.sleep(1)
+    mqtt.publish(t.steering_left, "0")
   else:
     mqtt.publish(t.steering_left, "0")
 
 
 @client.command()
-async def forward(ctx):
-  await drive(VELOCITY, 0)
+async def drive(ctx, *args):
+  s = 50
+  d = 0
+  for i in args[0]:
+    time.sleep(1)
+    if i == "<":
+      d = -50
+      await control(s, d)
+    if i == ">":
+      d = -50
+      await control(s, d)
+    if i == "s":
+      s = 0
+      await control(s, d)
+    if i == "r":
+      s = -50
+      await control(s, d)
+    if i == "f":
+      s = 50
+      await control(s, d)
+    if i == "b":
+      s = s*2
+      await control(s, d)
+  await control(0,0)
+
 
 @client.command()
-async def reverse(ctx):
-  await drive(-100, 0)
+async def go(ctx):
+  await control(VELOCITY, 0)
 
 @client.command()
-async def stop(ctx):
-  await drive(0, 0)
+async def rr(ctx):
+  await control(-VELOCITY, 0)
+
+@client.command()
+async def rrl(ctx):
+  await control(-VELOCITY, -DIRECTION)
+
+@client.command()
+async def rrr(ctx):
+  await control(-VELOCITY, DIRECTION)
+
+@client.command()
+async def s(ctx):
+  await control(0, 0)
 
 
 @client.command()
-async def left(ctx):
-  await drive(100, -50)
+async def gol(ctx):
+  await control(VELOCITY, -DIRECTION)
 
 @client.command()
-async def right(ctx):
-  await drive(100, 50)
+async def gor(ctx):
+  await control(VELOCITY, DIRECTION)
 
-
+@client.command()
+async def boost(ctx):
+  await control(100, 0)
+  await time.sleep(1)
+  await control(VELOCITY, 0)
 
 @client.event
 async def on_ready():
