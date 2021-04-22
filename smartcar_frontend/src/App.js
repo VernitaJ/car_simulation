fc2d14bf55e572517bfc56b38e60fb6218218afe
Axel-J-L <70752905+Axel-J-L@users.noreply.github.com>
2021-04-22 18:34:37 +0200

    Continue Camera-integration.
    Working on converting binary data to image.

--- smartcar_frontend/src/App.js
+++ smartcar_frontend/src/App.js
@@ -6,10 +6,12 @@ import LeaderBoard from "./Components/LeaderBoard";
 import Home from "./Components/Home";
 import Times from "./Components/Times";
 import Settings from "./Components/Settings";
+import CameraTest from "./Components/camera/CameraTest"
 
 function App() {
   return (
     <div className="App">
+      <CameraTest />
       <Router>
         <Route exact path="/" component={Home} />
         <Route exact path="/race" component={Race} />
