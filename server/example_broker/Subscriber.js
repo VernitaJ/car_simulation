69bde1278e95feffdee1488bc8cb6e5dbdc6c3ee
VernitaJ <vernitajones87@gmail.com>
2021-05-26 19:21:37 +0200

    restructuring

--- server/example_broker/Subscriber.js
+++
@@ -1,9 +0,0 @@
-const mqtt = require("mqtt")
-const client  = mqtt.connect("mqtt://localhost:1883")
-const topic = "/smartcar/camera"
-client.subscribe(topic)
-
-client.on("message", (topic, message) => {
-    message = message.toString()
-    console.log(message)
-})
